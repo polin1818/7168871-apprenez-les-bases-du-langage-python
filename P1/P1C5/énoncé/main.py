32d05491b5ae3bc1d755124d1e1b278ebe58bbb4
polin1818 <156616183+polin1818@users.noreply.github.com>
2024-07-05 15:14:37 +0200

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,15 @@
 # Écrivez votre code ici !
+nom="lorenzo"
+age=19
+taille=1.70
+est_etudiant=true 
+#afficher  le contenue des variables
+print(f"nom: {nom}")
+print(f"age: {age}")
+print(f"taille: {taille}")
+print(f"est_etudiant: {est_etudiant}")
+#afficher le type de variable
+print(f"Type nom: {nom}")
+print(f"Type age: {age}")
+print(f"Type taille: {taille}")
+print(f"Type est_etudiant: {est_etudiant}")
