618bc359feec948135c8d33bb2d9f814014228e1
polin1818 <156616183+polin1818@users.noreply.github.com>
2024-07-05 15:55:08 +0200

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,9 @@
 # Écrivez votre code ici !
+fruits=["pomme","banane","orange"]
+fruits.append(kiwi)
+print(fruits)
+fruits.remove("orange")
+print(fruits)
+fruits[1]="ananas"
+len(fruits)
+fruits.sort()
