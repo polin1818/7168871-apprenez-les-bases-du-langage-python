f66ebb553d039fa1584ab09a304ba721d4f154b6
polin1818 <156616183+polin1818@users.noreply.github.com>
2024-07-07 20:51:06 +0200

    tp main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+fruits = {"pomme": "rouge", "banane": "jaune", "orange": "orange"} 
+fruits["kiwi"] = "vert"
+couleur_banane = fruits["banane"] 
+fruits["pomme"] = "vert" 
+del fruits["banane"] 
+print(fruits.keys())# Écrivez votre code ici !
